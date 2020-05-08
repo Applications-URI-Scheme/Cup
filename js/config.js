97797b0fbd3997183c6a969f99d2e0ac2d4e3306
Playhi <000902play@gmail.com>
2020-05-08 15:56:04 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 // array of Monitor-specific API keys or Read-Only API keys to list all monitors
 var __apiKeys = [
 	'm784906436-c34c42e258e04b15f9e3d6e9', // https://www.urischeme.com/
+	'm784907145-fbb48426d6322c60f3d41ba4', // https://status.urischeme.com/
 ];
 
 // refresh interval (in seconds)
